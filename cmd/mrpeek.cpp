0410f34f0c1caa7c5b6a99d2abf2ee1d51d9d7b9
Max Pietsch <mail@maxpietsch.com>
2020-06-17 15:30:04 +0200

    mrpeek: added reslice adapter. not working yet

--- cmd/mrpeek.cpp
+++ cmd/mrpeek.cpp
@@ -1,8 +1,13 @@
 #include "command.h"
 #include "image.h"
 #include "algo/loop.h"
+#include "interp/linear.h"
+#include "filter/resize.h"
+// #include "adapter/regrid.h"
+
 #include "sixel.h"
 
+const char* interp_choices[] = { "nearest", "linear", "cubic", "sinc", NULL };
 using namespace MR;
 using namespace App;
 
@@ -43,8 +48,22 @@ void usage ()
   + Option ("crosshairs",
             "draw crosshairs at specified position")
   +   Argument ("x").type_integer(0)
-  +   Argument ("y").type_integer(0);
+  +   Argument ("y").type_integer(0)
 
+  + OptionGroup ("Regridding options")
+    + Option   ("image_scale", "scale the image size by the supplied factor")
+    + Argument ("factor").type_float()
+
+    + Option ("interp", "set the interpolation method to use when reslicing (choices: nearest, linear, cubic, sinc. Default: cubic).")
+    + Argument ("method").type_choice (interp_choices)
+
+    + Option ("oversample",
+        "set the amount of over-sampling (in the target space) to perform when regridding. This is particularly "
+        "relevant when downsamping a high-resolution image to a low-resolution image, to avoid aliasing artefacts. "
+        "This can consist of a single integer, or a comma-separated list of 3 integers if different oversampling "
+        "factors are desired along the different axes. Default is determined from ratio of voxel dimensions (disabled "
+        "for nearest-neighbour interpolation).")
+    + Argument ("factor").type_sequence_int();
 }
 
 
@@ -57,10 +76,14 @@ using value_type = float;
 
 void run ()
 {
-  auto image_in = Image<value_type>::open (argument[0]);
+  auto header_in = Header::open (argument[0]);
+  Image<value_type> image_in = header_in.get_image<value_type>();
 
   int axis = get_option_value ("axis", 2);
   int slice = get_option_value ("slice", image_in.size(axis)/2);
+  if (slice >= image_in.size(axis))
+    throw Exception("slice " + str(slice) + " exceeds image size (" + str(image_in.size(axis)) + ") in axis " + str(axis));
+
   float offset = 0.0f;
   float scale = 1.0f;
   auto opt = get_options ("scaling");
@@ -77,6 +100,17 @@ void run ()
     offset = -scale*min;
   }
 
+  int interp = 2;  // cubic
+  opt = get_options ("interp");
+  if (opt.size()) {
+    interp = opt[0][0];
+  }
+
+  vector<int> oversample = Adapter::AutoOverSample;
+  opt = get_options ("oversample");
+  if (opt.size()) {
+    oversample = opt[0][0];
+  }
 
   int x_axis, y_axis;
   bool x_forward, y_forward;
@@ -91,18 +125,39 @@ void run ()
   Sixel::ColourMap colourmap (100);
   colourmap.set_scaling (offset, scale);
 
-  const int x_dim = image_in.size(x_axis);
-  const int y_dim = image_in.size(y_axis);
+  Header header_target (header_in);
+  opt = get_options ("image_scale");
+  if (opt.size()) {
+    default_type image_scale (opt[0][0]);
+    vector<default_type> new_voxel_size (1.0, 3);
+    Eigen::Vector3 original_extent;
+    for (int d = 0; d < 3; ++d) {
+      original_extent[d] = header_in.size(d) * header_in.spacing(d);
+      new_voxel_size[d] = (header_in.size(d) * header_in.spacing(d)) / std::ceil (header_in.size(d) * image_scale);
+      header_target.size(d) = std::round (header_in.size(d) * header_in.spacing(d) / new_voxel_size[d] - 0.0001); // round down at .5
+      // Here we adjust the translation to ensure the image extent is centered wrt the original extent.
+      // This is important when the new voxel size is not an exact multiple of the original extent
+      for (size_t i = 0; i < 3; ++i)
+        header_target.transform()(i,3) += 0.5 * ((new_voxel_size[d] - header_target.spacing(d))  + (original_extent[d] - (header_target.size(d) * new_voxel_size[d]))) * header_target.transform()(i,d);
+      header_target.spacing(d) = new_voxel_size[d];
+    }
+  }
 
-  image_in.index(axis) = slice;
+  Adapter::Reslice<Interp::Linear, Image<value_type>> image_regrid(image_in, header_target);  // NoTransform, AutoOverSample, out_of_bounds_value
+
+  const int x_dim = image_regrid.size(x_axis);
+  const int y_dim = image_regrid.size(y_axis);
+
+  image_regrid.index(axis) = slice;
+
 
   Sixel::Encoder encoder (x_dim, y_dim, colourmap);
 
   for (int y = 0; y < y_dim; ++y) {
-    image_in.index(y_axis) = y_forward ? y : y_dim-1-y;
+    image_regrid.index(y_axis) = y_forward ? y : y_dim-1-y;
     for (int x = 0; x < x_dim; ++x) {
-      image_in.index(x_axis) = x_forward ? x : x_dim-1-x;
-      encoder(x, y, image_in.value());
+      image_regrid.index(x_axis) = x_forward ? x : x_dim-1-x;
+      encoder(x, y, image_regrid.value());
     }
   }
 
